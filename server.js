641a8e0369771c42b9ade9ed13d82bfa17dcf360
Samuel-dot-cloud <successam336@gmail.com>
2020-10-20 13:27:19 +0300

    Completing basic blog site

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/articles')
-const app = express();
 const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
+
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
@@ -13,6 +15,7 @@ mongoose.connect('mongodb://localhost/blog', {
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', async(req, res) => {
     const articles = await Article.find().sort({
